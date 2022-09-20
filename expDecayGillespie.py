fa24b1ed7a12db6fd10fa14008ae51e9d6226c01
FMagnani <federico.magnani5@studio.unibo.it>
2022-09-20 10:05:54 +0200

    Documented

--- expDecayGillespie.py
+++ expDecayGillespie.py
@@ -4,16 +4,20 @@ from gillespieModel import Reaction, GillespieModel
 from scipy.integrate import solve_ivp
 
 def main():
+    # Define reactions
     rate = 0.5
-    maxTime = 10
-    n0 = 1000
 
     decayReaction = Reaction("decay", [-1,+1], lambda s: s[0]*rate)
     nothingReaction = Reaction("nothing", [0,0], lambda s: s[0]*(1-rate) + s[1])
     groupNames = ["Alive", "Decayed"]
 
+    # Define simulation conditions
+    maxTime = 10
+    n0 = 1000
+
     fig, ax = plt.subplots(2,2)
 
+    # run some stochastic simluations
     for i in range(5):
         model = GillespieModel([decayReaction, nothingReaction], groupNames)
 
@@ -31,7 +35,7 @@ def main():
     ax[1,0].legend()
     ax[1,1].legend()
 
-    # deterministic
+    # deterministic solution
     tEval = np.linspace(0,maxTime, len(times))
     tInt = [0,maxTime]
     n0 = np.array([n0])
@@ -41,13 +45,6 @@ def main():
     ax[0,0].scatter(tEval, sol.y[0], color='r', s=1)
     ax[0,1].plot(tEval, sol.y[0], 'r')
 
-    # propensities
-#    fig1, ax = plt.subplots(2,1)
-#    ax[0].scatter(times[:], propensities[:,0], color='red', s=2, label="Propensity of decay vs time")
-#    ax[0].plot(times[:], np.sum(propensities, axis=1), label="Total propensity vs time")
-#    ax[0].legend()
-#    ax[1].plot(states[:,0], propensities[:,0], label="Propensity of decay vs number of alive particles")
-#    ax[1].legend()
     plt.show()
 
 if(__name__ == '__main__'):

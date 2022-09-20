77a1f5bcec6d7d6484bc14d9f0f13fc859f20c41
FMagnani <federico.magnani5@studio.unibo.it>
2022-09-20 17:23:36 +0200

    Tested

--- agedDecayGillespie.py
+++ agedDecayGillespie.py
@@ -3,17 +3,16 @@ import matplotlib.pyplot as plt
 from gillespieModel import Reaction, GillespieModel
 from scipy.integrate import solve_ivp
 
-
 class State:
-    def __init__(self, A0, D0, age0):
+    def __init__(self, A0, D0):
         self.A = A0
         self.D = D0
-        self.age = age0
 
-def mu(age):
+def mu(t):
+    # pdf Weibull / 1- cdf Weibull
     d = 0.03
     m = 2
-    return (m*d)*pow((age*d), m-1)
+    return (m*d)*pow((t*d), m-1)
 
 def pdfWeibull(t):
     d = 0.03
@@ -21,65 +20,61 @@ def pdfWeibull(t):
     return (m*d)*pow((t*d), m-1)*pow(np.e, -pow(-d*t, m))
 
 def cdfWeibull(t):
+    # Actually 1 - cdf
     d = 0.03
     m = 2
     return pow(np.e, -pow(-d*t, m))
 
-def decayPropensity(state):
-    return mu(state.age)*state.A
+def decayPropensity(state, t):
+    return mu(t)*state.A
 
-def nothingPropensity(state):
-    return (1-mu(state.age))*state.A + state.D
+def nothingPropensity(state, t):
+    return (1-mu(t))*state.A + state.D
 
 
 def main():
-
-#    t = np.linspace(0,100,50)
-#    fig, ax = plt.subplots(1,1)
-#    ax.plot(t, mu(t))
 
     # define model
     decayReaction = Reaction(
         name = "decay",
         propensity = decayPropensity,
-        apply = lambda s: State(s.A-1, s.D+1, s.age+1)
+        apply = lambda s: State(s.A-1, s.D+1)
     )
     nothingReaction = Reaction(
         name = "nothing",
         propensity = nothingPropensity,
-        apply = lambda s: State(s.A, s.D, s.age+1)
+        apply = lambda s: State(s.A, s.D)
     )
 
     model = GillespieModel([decayReaction, nothingReaction])
 
     # define Simulation
-    maxTime = 4
-    initState = State(1000, 0, 1)
+    maxTime = 50
+    A0 = 1000
+    initState = State(A0, 0)
 
     fig, ax = plt.subplots(1,2)
 
     # run some stochastic simluations
-    for i in range(5):
+    for i in range(3):
 
         times, states, reactions, propensities = model.simulate(initState, maxTime, i)
         alives = np.array([s.A for s in states])
-        ages = np.array([s.age for s in states])
         times = np.array(times)
         propensities = np.array(propensities)
         propensities = propensities[:,0]
 
-        ax[0].plot(ages, alives/1000, 'k', alpha=0.5, label="Number of particles vs time")
-        ax[1].plot(ages, propensities, 'r', alpha=0.5, label="Propensity of decay vs time")
+        ax[0].plot(times, alives, 'k', alpha=0.5, label="Number of particles vs time")
+        ax[1].plot(times, propensities, 'k', alpha=0.5, label="Propensity of decay vs time")
 
         ax[0].legend()
         ax[1].legend()
 
-    ax[0].plot(times, cdfWeibull(ages), 'r')
+    # deterministic
+    ax[0].plot(times, A0*cdfWeibull(times), 'r', label="Weibull cdf * A0")
+    ax[1].plot(times, A0*pdfWeibull(times), 'r', label="Weibull pdf * A0")
 
     plt.show()
-
-
-
 
 if(__name__ == '__main__'):
     main()

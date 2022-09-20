9213e89070316a8488114735cd80fe3f20b2bfdb
FMagnani <federico.magnani5@studio.unibo.it>
2022-09-20 21:08:05 +0200

    Fixed theoretical mistakes

--- expDecayGillespie.py
+++ expDecayGillespie.py
@@ -4,59 +4,55 @@ from gillespieModel import Reaction, GillespieModel
 from scipy.integrate import solve_ivp
 
 class State:
-    "Number of Alive and Decayed particles define the state"
-    def __init__(self, A0, D0):
-        self.A = A0
-        self.D = D0
+    def __init__(self, N0):
+        self.N = N0
 
 def main():
-    # Define reactions
-    rate = 0.5
 
+    rate = 0.5
     decayReaction = Reaction(
         "decay",
-        lambda s: s.A*rate,
-        lambda s: State(s.A-1, s.D+1)
-    )
-    nothingReaction = Reaction(
-        "nothing",
-        lambda s: s.A*(1-rate) + s.D,
-        lambda s: s
+        lambda s,t: s.N*rate,
+        lambda s: State(s.N-1)
     )
 
     # Define simulation conditions
     maxTime = 10
-    initState = State(1000, 0)
+    initState = State(1000)
 
     fig, ax = plt.subplots(2,2)
 
     # run some stochastic simluations
-    for i in range(5):
-        model = GillespieModel([decayReaction, nothingReaction])
+    for i in range(10):
+        model = GillespieModel([decayReaction])
 
         times, states, reactions, propensities = model.simulate(initState, maxTime, i)
-        states = [s.A for s in states]
+        states = [s.N for s in states]
         propensities = np.array(propensities)
 
-        ax[0,0].scatter(times, states, color='black', alpha=0.7, s=1.5, label="Number of particles vs time")
-        ax[0,1].plot(times, states, 'k', alpha=0.5, label="Number of particles vs time")
-        ax[1,1].plot(times, propensities[:,0], 'b', alpha=0.5, label="Propensity of decay vs time")
-        ax[1,0].scatter(times, propensities[:,0], color='blue', alpha=0.7, s=1.5, label="Propensity of decay vs time")
+        ax[0,0].scatter(times, states, color='black', alpha=0.7, s=1.5)
+        ax[0,1].plot(times, states, 'k', alpha=0.5)
+        ax[1,1].plot(times, propensities[:,0], 'k', alpha=0.5)
+        ax[1,0].scatter(times, propensities[:,0], color='black', alpha=0.7, s=1.5)
 
-    ax[0,0].legend()
-    ax[0,1].legend()
-    ax[1,0].legend()
-    ax[1,1].legend()
+    ax[0,0].set_title("Number of particles vs time")
+    ax[0,1].set_title("Number of particles vs time")
+    ax[1,0].set_title("Propensity of decay vs time")
+    ax[1,1].set_title("Propensity of decay vs time")
 
     # deterministic solution
     tEval = np.linspace(0,maxTime, len(times))
     tInt = [0,maxTime]
-    n0 = np.array([initState.A])
+    n0 = np.array([initState.N])
     def expDecay(t, y):
         return -rate*y
     sol = solve_ivp(expDecay, tInt, n0, t_eval=tEval)
-    ax[0,0].scatter(tEval, sol.y[0], color='r', s=1)
-    ax[0,1].plot(tEval, sol.y[0], 'r')
+    ax[0,0].scatter(tEval, sol.y[0], color='r', s=1, label="Deterministic", alpha=0.8)
+    ax[0,1].plot(tEval, sol.y[0], 'r', alpha=0.8)
+    ax[1,0].scatter(tEval, rate*sol.y[0], color='r', s=1, label="Deterministic", alpha=0.8)
+    ax[1,1].plot(tEval, rate*sol.y[0], 'r', alpha=0.8)
+    ax[0,0].legend()
+    ax[1,0].legend()
 
     plt.show()
 

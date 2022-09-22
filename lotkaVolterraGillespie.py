3c4510aa58d0cf0b4e4c665173bf5aa34470d70c
FMagnani <federico.magnani5@studio.unibo.it>
2022-09-22 11:29:56 +0200

    Fixed typos in apply functions

--- lotkaVolterraGillespie.py
+++ lotkaVolterraGillespie.py
@@ -15,33 +15,36 @@ def main():
 
     birth = Reaction(
         "preyBirth",
-        lambda s,t: 0.2*s.Prey,
-        lambda s: State(s.Prey+1, s.Pred)
+        lambda s,t: 1*s.Prey,
+        lambda s: State(s.Pred, s.Prey+1)
     )
     eat = Reaction(
         "preyEaten",
-        lambda s,t: 0.01*s.Prey*s.Pred,
-        lambda s: State(s.Prey-1, s.Pred+1)
+        lambda s,t: 0.001*s.Prey*s.Pred,
+        lambda s: State(s.Pred+1, s.Prey-1)
     )
     deathPred = Reaction(
         "predDeath",
-        lambda s,t: 0*s.Pred,
-        lambda s: State(s.Prey, s.Pred-1)
+        lambda s,t: 1*s.Pred,
+        lambda s: State(s.Pred-1, s.Prey)
     )
     deathPrey = Reaction(
         "preyDeath",
-        lambda s,t: 0*s.Prey,
-        lambda s: State(s.Prey-1, s.Pred)
+        lambda s,t: 0.5*s.Prey,
+        lambda s: State(s.Pred, s.Prey-1)
     )
 
     # Define simulation conditions
     maxTime = 30
-    initState = State(100, 100)
+    initState = State(1000, 1000)
 
-    fig, ax = plt.subplots(2,1)
+#    nSimulations = 1
+#    figArray = []
 
     # run some stochastic simluations
-    for _ in range(3):
+    for _ in range(2):
+
+        fig, ax = plt.subplots(2,1)
         model = GillespieModel([birth, eat, deathPred, deathPrey], isTerminalState)
 
         times, states, reactions, propensities = model.simulate(initState, maxTime)
@@ -49,12 +52,20 @@ def main():
         preds = [s.Pred for s in states]
         propensities = np.array(propensities)
 
-        ax[0].plot(times, preys, 'b', alpha=0.7, label="Preys")
+        ax[0].plot(times, preys, 'b', markersize=1.5, alpha=0.7, label="Preys")
         ax[0].plot(times, preds, 'r', alpha=0.7, label="Predators")
         ax[1].plot(times, propensities[:,0], 'b', alpha=0.7, label="Prey birth")
         ax[1].plot(times, propensities[:,1], 'r', alpha=0.7, label="Prey eaten")
         ax[1].plot(times, propensities[:,2], 'k', alpha=0.7, label="Predator death")
         ax[1].plot(times, propensities[:,3], 'g', alpha=0.7, label="Prey death")
+
+        ax[0].legend()
+        ax[1].legend()
+
+#    preys = np.array(preys)
+#    print(preys[1:]-preys[:-1])
+#    preds = np.array(preys)
+#    print(preds[1:]-preds[:-1])
 
 #    ax[0,0].set_title("Number of particles vs time")
 #    ax[0,1].set_title("Number of particles vs time")
